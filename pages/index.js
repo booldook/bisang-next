d1bc6c01a82eeacfe74c0eb40c03dc9eedc31c57
booldook <booldook@gmail.com>
2022-04-29 20:30:23 +0900

    props 문제 해결한 부분 올립니다. index.js 보시면 됩니다.

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,20 @@ import { useContext, useEffect } from "react";
 
 
 const Home = (props) => {
-  
+  console.log(props)
   return (
     <AppLayout>
       <h1>여기는 Index페이지 입니다.</h1>
-      {/* <div>{ pageProps && pageProps.books && pageProps.books[0] && pageProps.books[0].title }</div> */}
+      <div>{ props && props.books && props.books[0] && props.books[0].title }</div>
     </AppLayout>
   )
 }
 export const getServerSideProps = wrapper.getServerSideProps( store => async ({ req, res }) => {
   await store.dispatch(retrieveBook({ query: 'react' }));
+  const book = store.getState().book;
+  return {
+    props: { books: book.books } 
+  }
 });
 
 
