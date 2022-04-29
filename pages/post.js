630515973fbe16bc67bb938ff7b4f4570722f772
booldook <booldook@gmail.com>
2022-04-29 19:06:28 +0900

    next redux redux-persist

---
+++ pages/post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppLayout from '../components/AppLayout';
+
+const Post = () => {
+  return (
+  <AppLayout>
+    <h1>여기는 POST</h1>
+  </AppLayout>
+  )
+}
+
+export default Post
